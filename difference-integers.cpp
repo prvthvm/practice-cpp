ac0cbfece707dce330b69ed4d6e08b72c9dd7bd7
Pratham Tyagi <91729826+prvthvm@users.noreply.github.com>
2024-01-12 20:59:03 +0530

    Create difference-integers.cpp

---
+++ difference-integers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int diff (int x, int y){
+	
+	return x - y;
+}
+
+int main(){
+	int one,two;
+	cin>>one>>two;
+	cout<<diff(one,two)<<endl;
+
+   return 0;	
+}
