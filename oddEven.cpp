ef7d744d78ee04e853a4ed724cb4b5d41d8fd2b1
Pratham Tyagi <91729826+prvthvm@users.noreply.github.com>
2024-01-14 22:44:59 +0530

    Update oddEven.cpp

--- oddEven.cpp
+++ oddEven.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 
 string oddeven(int x){
-	
+
 	if(x % 2 == 0){
-	
+
 	    return "The number is even";
 	}
-	
+
 	else {
-		
+
 		return "The number is odd";
 	}
 }
 
 int main() {
-	
+
 	int num;
 	
+        cout<<"Input number to be checked: ";
 	cin>>num;
-	cout<<oddeven(num)<<endl;
-	
+	cout<<endl<<oddeven(num)<<endl;
+
 	return 0;
 }
