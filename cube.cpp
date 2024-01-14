9b154a2e9220e9dbdd1e99d6149d89b42155ec34
Pratham Tyagi <91729826+prvthvm@users.noreply.github.com>
2024-01-14 13:31:48 +0530

    Create cube.cpp
    Program to calculate the cube of inputed number.

---
+++ cube.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int cube(int x){
+	
+    return x*x*x;	
+}
+
+
+
+int main() {
+    
+    int num;
+    
+    cin>>num;
+    cout<<"Cube of the number: "<< cube(num)<< endl;
+    
+    
+    return 0;
+}
+   
+ 
