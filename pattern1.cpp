45db4bac4b0945ba2962e0243ae374f686c974e4
Pratham Tyagi <91729826+prvthvm@users.noreply.github.com>
2024-01-30 05:36:25 +0530

    Update pattern1.cpp

--- pattern1.cpp
+++ pattern1.cpp
@@ -21,7 +21,7 @@ void pattern(int n){
     n = 2 * n;
     for(int row = 0; row <= n; row++){
         
-        for(int col = 0; col < n; col++) {
+        for(int col = 0; col <= n; col++) {
 
             int everyindex = min(min(row,col), min(n-row,n-col));
             cout<<everyindex<<" ";
