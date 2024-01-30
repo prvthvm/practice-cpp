cc6b96619a6ea7cd46e305123aa57f14d38dbd91
Pratham Tyagi <91729826+prvthvm@users.noreply.github.com>
2024-01-30 05:34:11 +0530

    Create pattern1.cpp
    0 0 0 0 0 0 
    0 1 1 1 1 1 0
    0 1 2 2 21 0
    0 1 2 3 21 0
    0 1 2 2 21 0
    0 1 1 1 1 1 0
    0 0 0 0 0 0

---
+++ pattern1.cpp
@@ -0,0 +1,42 @@
+/* 
+#PROBLEM:
+Print this pattern-
+
+0 0 0 0 0 0 0
+0 1 1 1 1 1 0
+0 1 2 2 2 1 0
+0 1 2 3 2 1 0
+0 1 2 2 2 1 0
+0 1 1 1 1 1 0
+0 0 0 0 0 0 0
+
+Note:- Take pattern size from user.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int n){
+
+    n = 2 * n;
+    for(int row = 0; row <= n; row++){
+        
+        for(int col = 0; col < n; col++) {
+
+            int everyindex = min(min(row,col), min(n-row,n-col));
+            cout<<everyindex<<" ";
+          
+        }
+        cout<<endl;
+    }   
+}
+
+int main() {
+	
+	int num;
+    cin>>num;
+
+    pattern(num);
+  
+	return 0;
+}
